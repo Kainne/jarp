736113bac703288ef77264e4e04af2543f3893ab
chris williams <chilliams@gmail.com>
2013-11-17 19:07:21 -0500

    fix for badString

--- jarp.js
+++ jarp.js
@@ -16,12 +16,22 @@ var rd = readline.createInterface({
   terminal: false
 });
 
+var regen = function(word) {
+  var nonalphanum = '([^\w\d])';
+  word = word.replace(/\./, '\.');
+  return new RegExp(nonalphanum + word + nonalphanum);
+};
+
+var swap = function(line, oldword, newword) {
+  return line.replace(regen(oldword), '$1' + newword + '$2');
+};
+
 var jarp = function(line) {
-  line = line.replace(/main/, 'Main');
-  line = line.replace(/String/, 'string');
-  line = line.replace(/\.out\.println/, '.Console.WriteLine');
+  line = swap(line, 'main', 'Main');
+  line = swap(line, 'String', 'string');
+  line = swap(line, 'out.println', 'Console.WriteLine');
   return line;
-}
+};
 
 rd.on('line', function(line) {
   console.log( jarp(line) );
